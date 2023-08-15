4fc77f589e993d431cde5c4b3bd524ebc6503db7
Alper Gezer <a.yz07x@gmail.com>
2023-08-15 11:27:55 +0300

    Screenshot and Place it

--- RedditScraper.py
+++ RedditScraper.py
@@ -1,12 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 from gtts import gTTS
-import os
 import pyttsx3
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
-from moviepy.editor import VideoFileClip, AudioFileClip, TextClip
+from moviepy.editor import VideoFileClip, AudioFileClip, ImageClip
 from moviepy.video.tools.drawing import color_gradient
+from moviepy.config import change_settings
 import random
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"})
+
+def screenShotPost(url):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("start-maximized")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.TAG_NAME, "shreddit-post")))
+    post = driver.find_element(By.TAG_NAME, 'shreddit-post')
+    post.screenshot(filename="post.png")
+    driver.quit()
+
 
 def ScrapePostContent(url):
     html = requests.get(url).content
@@ -27,12 +48,14 @@ def ToSpeechPYTTSX3(content):
     # You can change the voice index to select different voices. 
     # In this case, using 1 for a male voice.
     engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 150)
+    engine.setProperty('rate', 175)
     
     text = content[1] + " " + content[2]
     # engine.say(text)
     engine.save_to_file(text, "example.mp3")
     engine.runAndWait()
+
+    return text
 
 
 def ToSpeechGTTS(content):
@@ -43,7 +66,7 @@ def ToSpeechGTTS(content):
 
 
 #VİDEO MAKER
-def GetVideoTXT(text_content, input_video_path = r"C:\Users\PC\Desktop\RedditVideoMaker\SubwaySurfer.mp4", input_audio_path = r"C:\Users\PC\Desktop\RedditVideoMaker\example.mp3", output_path = r"C:\Users\PC\Desktop\RedditVideoMaker\Output\output.mp4"):
+def GetVideoPNG(screenshotpath = r"C:\Users\PC\Desktop\RedditVideoMaker\post.png",input_video_path = r"C:\Users\PC\Desktop\RedditVideoMaker\SubwaySurfer.mp4", input_audio_path = r"C:\Users\PC\Desktop\RedditVideoMaker\example.mp3", output_path = r"C:\Users\PC\Desktop\RedditVideoMaker\Output\output.mp4"):
     video_clip = VideoFileClip(input_video_path)
     audio_clip = AudioFileClip(input_audio_path)
 
@@ -51,15 +74,30 @@ def GetVideoTXT(text_content, input_video_path = r"C:\Users\PC\Desktop\RedditVid
     start_time = random.uniform(0, max_start_time)
     cropped_video = video_clip.subclip(start_time, start_time + audio_clip.duration)
 
-    # Create a TextClip with the text content
-    txt_clip = TextClip(text_content, fontsize=30, color='white', size=cropped_video.size)
-    txt_clip = txt_clip.set_duration(cropped_video.duration)
+    final_video = cropped_video.set_audio(audio_clip)
+    final_video = final_video.volumex(0.8)
 
-    # Overlay the text onto the cropped video
-    video_with_text = CompositeVideoClip([cropped_video.set_audio(audio_clip), txt_clip.set_pos(('center', 'bottom'))])
+    png_image = ImageClip(screenshotpath)
+    png_duration = final_video.duration  # Adjust the duration of the PNG to match the video
+    png_image = png_image.set_duration(png_duration)
 
-    video_with_text.write_videofile(output_path, codec='libx264', audio_codec='aac')
+    # Calculate image width and height to fit with padding
+    padding = 30  # Adjust as needed
+    video_width, video_height = final_video.size
+    image_width = video_width - 2 * padding
+    image_height = png_image.h * image_width // png_image.w
 
+    png_image = png_image.resize(width=image_width, height=image_height)
+
+    # Position the PNG image with padding from the corner of the screen
+    x_pos = padding
+    y_pos = (video_height - image_height - padding)/2
+    positioned_image = png_image.set_position((x_pos, y_pos))
+
+    # Overlay the positioned image onto the video
+    final_video_with_overlay = CompositeVideoClip([final_video, positioned_image])
+
+    final_video_with_overlay.write_videofile(output_path, codec='libx264')
     video_clip.close()
     audio_clip.close()
 
@@ -71,15 +109,17 @@ def GetVideo(input_video_path = r"C:\Users\PC\Desktop\RedditVideoMaker\SubwaySur
     start_time = random.uniform(0, max_start_time)
     cropped_video = video_clip.subclip(start_time, start_time + audio_clip.duration)
     final_video = cropped_video.set_audio(audio_clip)
-    final_video.write_videofile(output_path, codec='libx264')
+    final_video = final_video.volumex(0.8)
 
+    final_video.write_videofile(output_path, codec='libx264')
     video_clip.close()
     audio_clip.close()
 
 
 def Scrape(url):
+    screenShotPost(url)
     result = ScrapePostContent(url)
-    text = ToSpeechGTTS(result)
+    text = ToSpeechPYTTSX3(result)
     print("Scraped Succesfully")
 
-    GetVideo()
+    GetVideoPNG()
--- main.py
+++ main.py
@@ -5,3 +5,5 @@ import RedditScraper
 link = input("Link: ")
 
 RedditScraper.Scrape(link)
+
+# https://www.reddit.com/r/offmychest/comments/15r8oua/today_is_my_birthday_no_one_remembered_me_and_i/
